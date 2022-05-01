19a6a340f61610ca830155b3f96efad12ff0c528
Wasilewski <jakub.wasilewski.ctr@sabre.com>
2022-05-01 11:02:19 +0200

    mongodb initial config fix

--- docker-entrypoint-initdb.d/mongo-init.js
+++ docker-entrypoint-initdb.d/mongo-init.js
@@ -1,2 +1,15 @@
+db.createUser(
+    {
+        user: "sa",
+        pwd: "password",
+        roles: [
+            {
+                role: "readWrite",
+                db: "scraping_db"
+            }
+        ]
+    }
+);
+
 db.createCollection('pages');
 db.createCollection('jobs');
